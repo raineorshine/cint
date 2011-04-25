3c378dd8bb299e5c3aa5aa762b725575db4424a8
Raine <raineorshine@gmail.com>
2011-04-25 13:02:17 -0600

    Added comment to guid().

--- scripts/rjs.1.4.js
+++ scripts/rjs.1.4.js
@@ -506,6 +506,7 @@ var hash = function(o) {
 	}
 };
 
+/** Generates a pseudo-random string that can be assumed to be unique. */
 var guid = (function() {
 	var S4 = function() {
 	   return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
