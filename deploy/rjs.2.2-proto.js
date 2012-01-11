6e3a6c3474b0f669b385a9390b8964fdf469329e
Raine <raineorshine@gmail.com>
2012-01-10 18:50:16 -0700

    Deploy v2.2-proto

---
+++ deploy/rjs.2.2-proto.js
@@ -0,0 +1,8 @@
+/** Installs string, number, array, and function methods from RJS to native object prototypes. 
+	@requires rjs.2.0.js
+*/
+
+RJS.install(String, RJS, ["supplant", "trim", "startsWith", "before", "after", "between", { repeatString: "repeat" }, "toTitleCase"]);
+RJS.install(Number, RJS, ["ordinal", { mapNumber: "map" }]);
+RJS.install(Array, RJS, ["map", "group", "orderedGroup", "tally", "contains", "strictContains", "unique", "reversed", "index", "rotate", "toObject", "find", "findByProperty", "filterBy" ]);
+RJS.install(Function, RJS, ["toInstance"]);
