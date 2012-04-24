d9b5cd57fcabc68fd2916fcfb6bd5d142a1f6ee0
Raine <raineorshine@gmail.com>
2012-04-24 11:42:01 -0600

    Add curry and currify to Function prototype.

--- rjs-proto.js
+++ rjs-proto.js
@@ -5,4 +5,4 @@
 RJS.install(String, RJS, ["supplant", "trim", "startsWith", "before", "after", "between", { repeatString: "repeat" }, "toTitleCase"]);
 RJS.install(Number, RJS, ["ordinal", { mapNumber: "map" }]);
 RJS.install(Array, RJS, ["map", "each", "pluck", "group", "orderedGroup", "tally", "contains", "strictContains", "unique", "reversed", "index", "rotate", "toObject", "find", "findByProperty", "filterBy" ]);
-RJS.install(Function, RJS, ["toInstance", "new"]);
+RJS.install(Function, RJS, ["curry", "currify", "toInstance", "new"]);
