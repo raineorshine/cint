46fa3062afec9958f3e0c184b87a5ba4bcffe20f
metaraine <raineorshine@gmail.com>
2014-02-20 23:29:50 -0700

    Add gulp with open,jshint,clean, and jsdoc. Fix linting errors in source. Rename docs folder. Remove numProperties (use Object.keys(...).length instead).

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+(function() {
+	'use strict';
+
+	var gulp =		require('gulp'),
+			// gutil = require('gulp-util'),
+			open =		require('gulp-open'),
+			jshint =	require('gulp-jshint'),
+			clean =		require('gulp-clean'),
+			jsdoc =		require('gulp-jsdoc');
+
+	gulp.task('default', function() {
+		gulp.src('./src/*.js')
+			.pipe(jshint('.jshintrc'))
+			.pipe(jshint.reporter('jshint-stylish'))
+			.pipe(jshint.reporter('fail'))
+		  .pipe(jsdoc('./docs'));
+	});
+
+	gulp.task('docs', function() {
+		gulp.src('./docs/index.html')
+			// how to access piped src?
+			.pipe(open('./docs/index.html', { app: 'Google Chrome' }));
+	});
+
+	gulp.task('clean', function() {
+	  gulp.src('./documentation-output', {read: false})
+	    .pipe(clean());
+	});
+})();
