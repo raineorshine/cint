f764c0f1667d9f361b6e85e27bfbe83a1a704764
metaraine <raineorshine@gmail.com>
2014-02-26 13:28:56 -0700

    Add uglify, rename, and template to gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,36 @@
 	'use strict';
 
 	var gulp =		require('gulp'),
-			// gutil = require('gulp-util'),
+			pkg = 		require('./package.json'),
 			open =		require('gulp-open'),
 			jshint =	require('gulp-jshint'),
 			clean =		require('gulp-clean'),
-			jsdoc =		require('gulp-jsdoc');
+			jsdoc =		require('gulp-jsdoc'),
+			uglify =	require('gulp-uglify'),
+			rename =	require('gulp-rename'),
+			template =require('gulp-template');
 
-	gulp.task('default', function() {
+	gulp.task('default', ['clean'], function() {
 
-		gulp.src('./src/*.js')
+		gulp.src('src/*.js')
 			// jshint not working now?
 			.pipe(jshint('.jshintrc'))
 			.pipe(jshint.reporter('jshint-stylish'))
 			.pipe(jshint.reporter('fail'))
 		  .pipe(jsdoc('./docs'));
+
+		gulp.src('src/*.js')
+			.pipe(template({ version: pkg.version, date: (new Date()).toUTCString() }, { interpolate: /{{([\s\S]+?)}}/g }))
+	    .pipe(rename({ basename: 'latest' }))
+	    .pipe(gulp.dest('dist'))
+	    .pipe(rename({ basename: 'cint-' + pkg.version }))
+	    .pipe(gulp.dest('dist'))
+	    .pipe(uglify())
+	    .pipe(rename({ suffix:'.min' }))
+	    .pipe(gulp.dest('dist'))
+	    .pipe(rename({ basename: 'cint-' + pkg.version }))
+	    .pipe(rename({ suffix:'.min' }))
+	    .pipe(gulp.dest('dist'));
 	});
 
 	gulp.task('docs', function() {
@@ -26,7 +42,8 @@
 	});
 
 	gulp.task('clean', function() {
-	  gulp.src('./docs', {read: false})
-	    .pipe(clean());
+	  gulp.src(['docs'], {read: false})
+	    .pipe(clean({force:true}));
 	});
+
 })();
