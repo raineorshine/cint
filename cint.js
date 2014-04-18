596a326607b512f686193165394e0d38e7785feb
metaraine <raineorshine@gmail.com>
2014-04-17 18:20:28 -0600

    Move jsdocs into its own gulp task and rebuild.

--- cint.js
+++ cint.js
@@ -1,7 +1,7 @@
 /** A Javascript utility belt with an emphasis on Functional Programming.
 	@module cint
 	@author Raine Lourie
-	@version v6.0.0 (Fri, 18 Apr 2014 00:13:01 GMT)
+	@version v6.0.0 (Fri, 18 Apr 2014 00:19:59 GMT)
 */
 cint = (function() {
 	'use strict';
--- gulpfile.js
+++ gulpfile.js
@@ -17,28 +17,23 @@
 			destPath = './',
 			docsPath = 'docs';
 
+	gulp.task('docs', function() {
+		gulp.src(srcPath)
+		  .pipe(jsdoc(docsPath))
+	});
+
 	gulp.task('default', ['clean'], function() {
 
 		gulp.src(srcPath)
-			// jshint not working now?
+			// is jshint working?
 			.pipe(jshint('.jshintrc'))
 			.pipe(jshint.reporter('jshint-stylish'))
 			.pipe(jshint.reporter('fail'))
-		  .pipe(jsdoc(docsPath))
-
-		gulp.src(srcPath)
 			.pipe(header(fs.readFileSync('header.ejs'), extend(pkg, { buildtime: (new Date()).toUTCString() })))
 	    .pipe(gulp.dest(destPath))
 	    .pipe(uglify())
 	    .pipe(rename({ suffix: '.min' }))
 	    .pipe(gulp.dest(destPath))
-	});
-
-	gulp.task('docs', function() {
-		gulp.src('./docs/index.html')
-			// how to access piped src?
-			// TODO: output stderr
-			.pipe(open('./docs/index.html', { app: 'Google Chrome' }));
 	});
 
 	gulp.task('clean', function() {
